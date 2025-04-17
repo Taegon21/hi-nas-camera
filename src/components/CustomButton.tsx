90f4a00e80b444ce45b99b41df70190d558ab1e3
TaegeonKim <aksen5240@gmail.com>
2025-04-17 17:54:08 +0900

    refactor: CustomButton 컴포넌트에 onClick, type 속성 추가

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -3,12 +3,20 @@ import { twMerge as tw } from "tailwind-merge";
 interface CustomButtonProps {
   buttonText: string;
   className?: string;
+  onClick?: () => void;
+  type?: "submit" | "button" | "reset";
 }
 
-export const CustomButton = ({ buttonText, className }: CustomButtonProps) => {
+export const CustomButton = ({
+  buttonText,
+  className,
+  onClick,
+  type = "submit",
+}: CustomButtonProps) => {
   return (
     <button
-      type="submit"
+      type={type}
+      onClick={onClick}
       className={tw(
         "bg-primary text-primary-foreground hover:bg-primary/90 rounded-md px-6 py-3 font-semibold",
         className
