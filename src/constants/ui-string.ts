3da58deb0dd5957efb553af0333c90f93e302eb0
TaegeonKim <aksen5240@gmail.com>
2025-04-18 13:56:45 +0900

    feat: UI 문자열 중앙 관리를 위한 상수 파일 추가

---
+++ src/constants/ui-string.ts
@@ -0,0 +1,8 @@
+export const UI_STRINGS = {
+  BRAND_NAME: "HiNAS Camera",
+  TITLE: "Camera Parameter",
+  BUTTON_SUBMIT: "Submit",
+  BUTTON_CLOSE: "Close",
+  THEME_DARK: "Dark",
+  THEME_LIGHT: "Light",
+};
